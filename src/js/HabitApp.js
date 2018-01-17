5ccf239058d19b91ccad869873b2ac746ed12030
John Ovalle <johnovalle@gmail.com>
2018-01-17 00:43:25 -0500

    combine groups, habits, and entires into nested group objects

--- src/js/HabitApp.js
+++ src/js/HabitApp.js
@@ -3,8 +3,28 @@ import data from './temp-data';
 export default class HabitApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    console.log(data);
+    this.state = {groups: this.sortHabits(data)};
+
+    console.log(this.state.groups);
+  }
+  sortHabits(data){
+    let habits = this.groupItemByContainer(data.habits, "entires", data.entires, "habitId");
+
+    data.groups.push({id: null, habits: []});
+    let groups = this.groupItemByContainer(data.groups, "habits", habits, "groupId");
+
+    return groups;
+  }
+  groupItemByContainer(containers, type, items, fKey) {
+    let containerMap = {};
+    for(let i = 0; i < containers.length; i++) {
+      containerMap[containers[i].id] = containers[i];
+    }
+
+    for(let i = 0; i < items.length; i++) {
+      containerMap[items[i][fKey]][type].push(items[i]);
+    }
+    return containers;
   }
   render() {
     return (
